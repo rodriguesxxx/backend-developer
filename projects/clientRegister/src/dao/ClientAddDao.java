e02615d70513f1e32f1f47b324015b4b2349b584
blkd <blkd.html@gmail.com>
2023-08-04 06:56:21 -0300

    projeto de crud na memoria

---
+++ projects/clientRegister/src/dao/ClientAddDao.java
@@ -0,0 +1,56 @@
+package dao;
+
+import java.util.Random;
+
+import models.Client;
+import database.ClientDB;
+
+public class ClientSetDao{
+
+
+
+    //PUBLIC METHODS
+    public static  void add(Client client){
+
+
+        if( isUniqueCpf(client.getCpf()) ){
+            client.setId( generateId() );
+            ClientDB.add(client);
+
+        }
+    }
+
+    //PRIVATE METHODS
+    private static boolean isUniqueCpf(String cpf){
+
+       if( ClientSearchDao.getClientByCpf(cpf) == null) {
+           return true;
+       }
+
+       return false;
+
+    }
+
+    private static Integer generateId(){
+
+        Random generator = new Random();
+
+        Integer id;
+
+        do{
+            id = generator.nextInt( 9999 );
+        } while( !isUniqueId(id) );
+
+        return id;
+    }
+
+    private static boolean isUniqueId(Integer id){
+
+        if( ClientSearchDao.getClietById(id) == null ){
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ projects/clientRegister/src/dao/ClientDelDao.java
@@ -0,0 +1,2 @@
+package dao;public class ClientDelDao {
+}
