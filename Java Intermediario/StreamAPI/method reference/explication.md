487e55f06832ba0316165e108e22e1145777c0a3
Daniel Rodrigues <rodriguex.daniel89@gmail.com>
2023-10-03 10:29:47 -0300

    aula sobre method reference

---
+++ Java Intermediario/StreamAPI/method reference/explication.md
@@ -0,0 +1,4 @@
+# Method Reference 
+
+- Method Reference é um novo recurso do Java 8 que permite fazer referência a um método ou construtor de uma classe (de forma funcional) e assim indicar que ele deve ser utilizado num ponto específico do código, deixando-o mais simples e legível.
+- Para utilizá-lo, basta informar uma classe ou referência seguida do símbolo “::” e o nome do método sem os parênteses no final.
---
+++ Java Intermediario/StreamAPI/src/examples/methodReference/Main.java
@@ -0,0 +1,27 @@
+package examples.methodReference;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Rapper rapper1 = new Rapper("Drake - Yebbas", 200000);
+        Rapper rapper2 = new Rapper("Look At me", 5071003);
+        Rapper rapper3 = new Rapper("Tupac - Ambr", 1000000);
+        Rapper rapper4 = new Rapper("Jocl Flores", 5000000);
+
+        List<Rapper> listRappers = new ArrayList<>(Arrays.asList(rapper1, rapper2, rapper3, rapper4));
+        
+        try{
+            List<Rapper> sortedRappers = Rapper.sortByViews(listRappers);
+            System.out.println(sortedRappers.toString());
+
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
