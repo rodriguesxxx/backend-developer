1770cf46e3cefb4712d627e453f6ef29abf165d2
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2025-01-16 19:12:11 -0300

    [Alg. e Est. Dados]: [ORDENAÇÃO] Bidirectional Bubble sort

--- Estrutura de dados e Algoritmos/Ordenação/BUBBLE SORT/main.c
+++ Estrutura de dados e Algoritmos/Ordenação/BUBBLE SORT/main.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
-#include<stdlib.h>
+
+void bidirectional_sort(int arr[], size_t size) {
+    int swapped;
+    do {
+        swapped = 0;
+        for (size_t i = 0; i < size - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int aux = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = aux;
+                swapped = 1;
+            }
+        }
+
+        for (size_t i = size - 1; i > 0; i--) {
+            if (arr[i] < arr[i - 1]) {
+                int aux = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = aux;
+                swapped = 1;
+            }
+        }
+    } while (swapped);
+}
 
 void sort(int arr[], size_t size) {
     for(int j = 0; j < size - 1; j++) {
@@ -19,7 +42,7 @@ int main() {
     int arr[] = {4, 3, 2, 1};
     size_t size = sizeof(arr)/sizeof(arr[0]);
 
-    sort(arr, size);
+    bidirectional_sort(arr, size);
 
     for(int i = 0; i < size; i++) {
         printf("%d\n", arr[i]);
